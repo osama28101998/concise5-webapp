98150f038093cc2f59a7f09ab3d5a133ec198866
Muhammad Osama Shahzad <osamashahzad603@gmail.com>
2025-06-04 12:13:09 +0500

    revert changes.

--- next.config.mjs
+++ next.config.mjs
@@ -1,64 +1,57 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "http://35.202.100.254/oe/:path*",
-      },
-    ];
-  },
+  
   webpack: (config, { isServer }) => {
     if (isServer) {
       // Stub out the canvas module for server-side builds
-      config.resolve.alias["canvas"] = false;
+      config.resolve.alias['canvas'] = false;
     }
     return config;
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+    eslint: {
+        ignoreDuringBuilds:true
+    },
+    async headers() {
+        return [
+          {
+            source: "/api/(.*)",
+            headers: [
+              {
+                key: "Access-Control-Allow-Origin",
+                value: "*",
+              },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET, POST, PUT, DELETE, OPTIONS",
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value:
+                  "Content-Type, Authorization, x-refresh-token,x-assistant-id",
+              },
+            ],
+          },
+          {
+            source: "/api/(.*)/(.*)",
+            headers: [
+              {
+                key: "Access-Control-Allow-Origin",
+                value: "*",
+              },
+              {
+                key: "Access-Control-Allow-Methods",
+                value: "GET, POST, PUT, DELETE, OPTIONS",
+              },
+              {
+                key: "Access-Control-Allow-Headers",
+                value:
+                  "Content-Type, Authorization, x-refresh-token,x-assistant-id",
+              },
+            ],
+          },
+        ];
   },
-  async headers() {
-    return [
-      {
-        source: "/api/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "Content-Type, Authorization, x-refresh-token,x-assistant-id",
-          },
-        ],
-      },
-      {
-        source: "/api/(.*)/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "Content-Type, Authorization, x-refresh-token,x-assistant-id",
-          },
-        ],
-      },
-    ];
-  },
-  devIndicators: false,
+    devIndicators:false
 };
 
 export default nextConfig;
